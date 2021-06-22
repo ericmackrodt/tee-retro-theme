c19ce9a8b46bac2bc2a1c40336493a7bf783d900
Eric Mackrodt <eric.mackrodt@safetyculture.io>
2021-06-22 14:26:21 +1000

    update seo

--- src/post.php
+++ src/post.php
@@ -1,25 +1,26 @@
 <?php $this->layout(withVariant('layout'), ['title' => $this->e($data->title)]) ?>
 <?php $this->push('seo') ?>
+<?php $image = "https://www.ericexperiment.com/contents/posts/" . $data->slug . '/' . $data->image; ?>
 <meta name="description" content="<?= $this->e($data->description); ?>">
-<link rel="canonical" href="http://www.theericexperiment.com/post/<?= $this->e($data->slug); ?>" />
+<link rel="canonical" href="https://www.ericexperiment.com/post/<?= $this->e($data->slug); ?>" />
 <meta property="og:locale" content="en_AU" />
 <meta property="og:type" content="article" />
 <meta property="og:title" content="<?= $this->e($data->title); ?>" />
 <meta property="og:description" content="<?= $this->e($data->description); ?>" />
-<meta property="og:url" content="http://www.theericexperiment.com/post/<?= $this->e($data->slug); ?>" />
+<meta property="og:url" content="https://www.ericexperiment.com/post/<?= $this->e($data->slug); ?>" />
 <meta property="og:site_name" content="The Eric Experiment" />
 <meta property="article:section" content="Blog" />
-<meta property="article:published_time" content="@(model.metadata.date)T12:10:00+00:00" />
-<meta property="article:modified_time" content="@(model.metadata.date)T10:05:01+00:00" />
-<meta property="og:updated_time" content="@(model.metadata.date)T10:05:01+00:00" />
-<meta property="og:image" content="http://www.theericexperiment.com/post/asset/143/143/@image?fit=cover" />
-<meta property="og:image:secure_url" content="https://www.theericexperiment.com/post/asset/143/143/@image?fit=cover" />
+<meta property="article:published_time" content="<?= $this->e($data->date); ?>T12:10:00+00:00" />
+<meta property="article:modified_time" content="<?= $this->e($data->date); ?>T10:05:01+00:00" />
+<meta property="og:updated_time" content="<?= $this->e($data->date); ?>T10:05:01+00:00" />
+<meta property="og:image" content="<?= $image ?>" />
+<meta property="og:image:secure_url" content="<?= $image ?>" />
 <meta property="og:image:width" content="143" />
 <meta property="og:image:height" content="80" />
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:description" content="<?= $this->e($data->description); ?>" />
 <meta name="twitter:title" content="<?= $this->e($data->title); ?>" />
-<meta name="twitter:image" content="https://www.theericexperiment.com/post/asset/143/143/@image?fit=cover" />
+<meta name="twitter:image" content="<?= $image ?>" />
 <?php $this->end() ?>
 
 <img src="/templates/retro/public/nothing.gif" width="1" height="5"><br><img src="/templates/retro/public/black_pixel.gif" width="100%" height="1"><br><img src="/templates/retro/public/nothing.gif" width="1" height="5"><br>
