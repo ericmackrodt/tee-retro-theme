619f2eb0a1d74302580c15d75de7e9df37c85c0f
Eric Mackrodt <ericmackrodt@gmail.com>
2021-07-08 14:10:45 +1000

    compress other images

--- src/gallery.php
+++ src/gallery.php
@@ -1,5 +1,5 @@
 <p>
   <?php foreach ($images as $i => $image) : ?>
-    <a href="/gallery/<?= $page_slug ?>/<?= $id ?>?img=<?= $i ?>"><img src="/img.php?p=<?= $image->url ?>&w=120&fit=cover&quality=50" border="0" /><img src="/templates/retro/public/nothing.gif" width="5" height="90" border="0"></a>
+    <a href="/gallery/<?= $page_slug ?>/<?= $id ?>?img=<?= $i ?>"><img src="/img.php?p=<?= $image->url ?>&w=120&fit=cover&compression=50" border="0" /><img src="/templates/retro/public/nothing.gif" width="5" height="90" border="0"></a>
   <?php endforeach; ?>
 </p>
--- src/youtube.php
+++ src/youtube.php
@@ -1,3 +1,3 @@
 <center>
-  <a href="<?= $full_url ?>" target="_blank"><img src="/img.php?p=https://img.youtube.com/vi/<?= $this->e($videoId) ?>/0.jpg&aspectRatio=16:9" alt="%5" border="0" /></a>
+  <a href="<?= $full_url ?>" target="_blank"><img src="/img.php?p=https://img.youtube.com/vi/<?= $this->e($videoId) ?>/0.jpg&aspectRatio=16:9&w=350&compression=50" alt="%5" border="0" /></a>
 </center>
