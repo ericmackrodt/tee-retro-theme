8eaf0d89f8b5ab028870ba347ba24e9a1e8e8d0c
Eric Mackrodt <ericmackrodt@gmail.com>
2021-07-08 10:58:54 +1000

    Load menu icons directly

--- src/main-menu.php
+++ src/main-menu.php
@@ -2,7 +2,7 @@
 <table border="0" cellspacing="0" cellpadding="5">
   <tr>
     <?php foreach ($mainMenu as $key => $item) : ?>
-      <td align="center"><a href="<?= $item->path ?>"><img src="/img.php?p=<?= $item->icon ?>&output=gif" border="0"><br><font size="-1"><?= $item->label ?></font></a></td>
+      <td align="center"><a href="<?= $item->path ?>"><img src="<?= $item->icon ?>" border="0"><br><font size="-1"><?= $item->label ?></font></a></td>
     <?php endforeach; ?>
   </tr>
 </table>
