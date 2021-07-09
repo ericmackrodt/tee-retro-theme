8ec0c96e9293b0304c1c5418ec1fe5ad0e8e72b7
Eric Mackrodt <ericmackrodt@gmail.com>
2021-07-09 10:42:34 +1000

    Improve main menu

--- src/main-menu.php
+++ src/main-menu.php
@@ -2,22 +2,32 @@
 $per_row = 4;
 $count = count($mainMenu);
 $rows = ceil($count / 4);
-$index = 0;
+$offset = 0;
 ?>
 <table border="0" cellspacing="0" cellpadding="5">
   <?php for ($i = 1; $i <= $rows; $i++) : ?>
     <tr>
-      <?php $count = 0 ?>
-      <?php while ($count < $per_row) : ?>
-        <?php $item = $mainMenu[$index]; ?>
-        <td align="center"><a href="<?= $item->path ?>"><img src="<?= $item->icon ?>" border="0"><br>
-            <font size="-1"><?= $item->label ?></font>
-          </a></td>
-        <?php
-        $count++;
-        $index++;
-        ?>
-      <?php endwhile; ?>
+      <?php
+      $slice = array_slice($mainMenu, $offset, $per_row);
+      $slice_count = count($slice);
+      $items_to_add = $per_row - $slice_count;
+
+      if ($items_to_add > 0) {
+        $empty_array = array_fill(0, $per_row - $slice_count, null);
+        array_unshift($slice, $empty_array);
+      }
+      ?>
+
+      <?php foreach ($slice as $item) : ?>
+        <td align="center">
+          <?php if ($item != null) : ?>
+            <a href="<?= $item->path ?>"><img src="<?= $item->icon ?>" border="0"><br>
+              <font size="-1"><?= $item->label ?></font>
+            </a>
+          <?php endif; ?>
+        </td>
+      <?php endforeach; ?>
     </tr>
+    <?php $offset = $offset + $per_row; ?>
   <?php endfor; ?>
 </table>
